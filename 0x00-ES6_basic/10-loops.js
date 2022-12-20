a200258c2083d5bdbeab5a6c7ee74d985272ae23
SamanSurafel17 <samanmobile168@gmail.com>
2022-12-20 12:57:25 -0500

    alx frontend javascript es6

--- 0x00-ES6_basic/10-loops.js
+++
@@ -1,8 +0,0 @@
-export default function appendToEachArrayValue(array, appendString) {
-  const arry1 = [];
-  for (const idx of array) {
-    arry1.push(appendString + idx);
-  }
-
-  return arry1;
-}
--- 0x00-ES6_basic/11-createEmployeesObject.js
+++
@@ -1,6 +0,0 @@
-export default function createEmployeesObject(departmentName, employees) {
-  const newObj = {
-    [departmentName]: [...employees],
-  };
-  return newObj;
-}
