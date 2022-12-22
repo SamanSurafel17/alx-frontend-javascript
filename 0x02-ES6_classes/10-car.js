8d0a4d210c2c748c5e34e4703539539be6c86029
SamanSurafel17 <samanmobile168@gmail.com>
2022-12-22 13:31:31 -0500

    car

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,12 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    const Obj = this.constructor[Symbol.species] || this.constructor;
+    return new Obj();
+  }
+}
