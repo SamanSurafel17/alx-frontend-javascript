74de28371ffafffb5a27cad11c8abf990d8a355c
SamanSurafel17 <samanmobile168@gmail.com>
2022-12-23 13:24:51 -0500

    arrow

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,5 +1,6 @@
 export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
   const self = this;
   this.addNeighborhood = (newNeighborhood) => {
     self.sanFranciscoNeighborhoods.push(newNeighborhood);
