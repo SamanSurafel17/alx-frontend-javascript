175470ff33678d917bdcd76d635579ec5821b95c
SamanSurafel17 <samanmobile168@gmail.com>
2023-01-01 13:40:04 -0500

    gpt

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,16 +1,12 @@
-/* eslint-disable */
+// Rewritten
 import getBudgetObject from './7-getBudgetObject.js';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars(income) {
-      return `$${income}`;
-    },
-    getIncomeInEuros(income) {
-      return `${income} euros`;
-    },
+    getIncomeInDollars: income => `$${income}`,
+    getIncomeInEuros: income => `${income} euros`,
   };
 
   return fullBudget;
