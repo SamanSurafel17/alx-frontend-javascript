6a8dc414b1d0cf110c84af05813742a0080fa9e1
SamanSurafel17 <samanmobile168@gmail.com>
2023-01-03 13:05:31 -0500

    update uniq itesm

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,9 @@
+export default function updateUniqueItems(groceries) {
+  if (!(groceries instanceof Map)) throw Error('Cannot process');
+
+  groceries.forEach((value, key) => {
+    if (value === 1) groceries.set(key, 100);
+  });
+
+  return groceries;
+}
