c5fe1dd5d99068785880a15b91435b719751a969
SamanSurafel17 <samanmobile168@gmail.com>
2023-01-04 14:02:28 -0500

    type script

--- 0x04-TypeScript/0x04-TypeScript/task_3/js/crud.d.ts
+++
@@ -1,5 +0,0 @@
-import { RowID, RowElement} from './interface';
-
-declare function insertRow(row: RowElement) : number;
-declare function deleteRow(rowId: RowID): undefined;
-declare function updateRow(rowId: RowID, row: RowElement): number;
